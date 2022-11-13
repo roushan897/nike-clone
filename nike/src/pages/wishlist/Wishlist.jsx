09b78c8909e9cc17e509273e538df15c774fceb1
roy897 <roushangupta85@gmail.com>
2022-11-13 18:57:05 +0530

    wishlist done

--- nike/src/pages/wishlist/Wishlist.jsx
+++ nike/src/pages/wishlist/Wishlist.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import styles from "./wishlist.module.css";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import { Navbar } from "../../components/navbar/Navbar";
 import { Footer } from "../../components/Footer";
 import { WishlistContextProvider } from "../../context/Wishlist";
 import { useContext } from "react";
 import { ProductTable } from "../../components/ProductTable";
+import emptylist from '../../assets/empty-wishlist.png';
 
 export const Wishlist = () => {
   const { wishlist } = useContext(WishlistContextProvider);
-
+ 
   return (
     <Box className={styles.wishlistContainer}>
       <Navbar />
       <Box minH='500px'>
-        <ProductTable {...{hello: '', shoesData: wishlist, reCart: "yes"}} />
+        { wishlist.length>0? <ProductTable {...{hello: '', shoesData: wishlist, reCart: "yes"}} />        
+          : <Flex justify='center' align='center'>
+             <Image src={emptylist} alt='empty wishlist' />
+          </Flex>        
+        }        
       </Box>
       <Box>
         <Footer />
